2a0f7c6bb6c807303c1ee58b6e13462c9bc4e35c
Vijay <vijay@example.com>
2026-05-23 12:14:40 +0530

    initial commit

--- src/components/axios.jsx
+++ src/components/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:8080" });
+const api = axios.create({ baseURL: "https://saas-pos-backend-m8et.onrender.com" });
 
 api.interceptors.request.use((config) => {
   const tenant = localStorage.getItem("tenant");
