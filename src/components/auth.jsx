f55c2db00788ac2db5165a69c83132695c2b7169
Vijay <vijay@example.com>
2026-05-23 12:21:19 +0530

    initial commit

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -90,7 +90,7 @@ function Auth() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:8080/auth/login", {
+      const res = await fetch("https://saas-pos-backend-m8et.onrender.com/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(loginData),
